dd81ba2fbebfcfd57906d6797b281b474d581cfe
andreclerigo <andreclerigo@outlook.com>
2023-03-06 19:23:24 +0000

    sd pratica content

---
+++ SD/Prática/threadBasics/testPreemptiveness/Variable.java
@@ -0,0 +1,14 @@
+/**
+ *    Shared variable.
+ *    Access to it is public.
+ */
+
+public class Variable
+{
+  public volatile int a;                                   // shared access variable
+
+  public Variable (int a)
+  {
+    this.a = a;
+  }
+}
