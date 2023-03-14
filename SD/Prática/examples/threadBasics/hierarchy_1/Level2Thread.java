e9b3801fdc715512eb5274e002a073df5e542ecd
andreclerigo <andreclerigo@outlook.com>
2023-03-14 22:08:44 +0000

    sd thread examples

---
+++ SD/Prática/examples/threadBasics/hierarchy_1/Level2Thread.java
@@ -0,0 +1,20 @@
+/**
+ *    Level 2 thread.
+ */
+
+public class Level2Thread extends Thread
+{
+  private ReportStatus rps;                                          // info about threads internal state
+
+  public Level2Thread (ThreadGroup thrG, String tName, ReportStatus rps)
+  {
+    super (thrG, tName);
+    this.rps = rps;
+  }
+
+  @Override
+  public void run ()
+  {
+    rps.report ();
+  }
+}
---
+++ SD/Prática/examples/threadSynchronization/producerConsumer/ProducerConsumer5/ConditionVar.java
@@ -0,0 +1,12 @@
+/**
+ *    General description:
+ *       definition of the basic operations that can be peerformend on a condition variable.
+ *
+ */
+
+public interface ConditionVar
+{
+   public void await () throws IllegalThreadStateException, InterruptedException;
+   public void signal () throws IllegalThreadStateException;
+   public void signalAll () throws IllegalThreadStateException;
+}
